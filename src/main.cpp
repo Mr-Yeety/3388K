d39946acbeac3360527b30ddf6b806cad2fc0966
Mr-Yeety <98505470+Mr-Yeety@users.noreply.github.com>
2023-11-02 09:29:46 -0600

    Start

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       joshi                                                     */
+/*    Created:      11/2/2023, 9:28:24 AM                                     */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+#include "vex.h"
+#include <iostream>
+
+using namespace vex;
+
+
+// A global instance of vex::brain used for printing to the V5 brain screen
+vex::brain Brain;
+vex::controller Controller;
+
+
+motor driveLeft = motor(PORT1, ratio18_1, true);
+motor driveRight = motor(PORT2, ratio18_1, false);
+
+
+
+int main() {
+
+while (1)
+{
+    driveLeft.spin(vex::directionType::fwd, Controller.Axis3.value(), vex::velocityUnits::pct);
+    driveRight.spin(vex::directionType::fwd, Controller.Axis2.value(), vex::velocityUnits::pct);
+
+}
+}
